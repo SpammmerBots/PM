71c4e014a9de60b140aa03ef8ad907a94cce608a
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-12 04:24:34 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,20 @@ client1.channels.get('522375285761507329').send('..!daily <@411137717884289024>'
 client1.channels.get('522375285761507329').send('..!rep <@411137717884289024>');
 		  }, 61360000);
 });
-
+ar interval = setInterval (function () {
+client1.channels.get('522375285761507329').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
+		  }, 240000);
+  
+	      var interval1 = setInterval (function () {
+  client1.channels.get('522375285761507329').send('#daily');
+  client.channels.get('522375285761507329').send('#rep <@411137717884289024>');  
+		  }, 173600000);
+  
+	      var interval1 = setInterval (function () {
+client1.channels.get('522375285761507329').send('..!daily <@411137717884289024>');
+client1.channels.get('522375285761507329').send('..!rep <@411137717884289024>');
+		  }, 122720000);
+});
 client1.login(process.env.STYLE_BOT1);
 /////////
 
