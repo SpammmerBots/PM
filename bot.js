258dfb1514bd53e6d125862cc37677e183a580c0
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-12 04:16:07 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,13 +12,33 @@ client1.channels.get('522375285761507329').send('لا إله الا الله, س
   client.channels.get('522375285761507329').send('#rep <@411137717884289024>');  
 		  }, 86800000);
   
-	      var interval2 = setInterval (function () {
+	      var interval1 = setInterval (function () {
 client1.channels.get('522375285761507329').send('..!daily <@411137717884289024>');
 client1.channels.get('522375285761507329').send('..!rep <@411137717884289024>');
 		  }, 61360000);
 });
 
 client1.login(process.env.STYLE_BOT1);
+/////////
+client2.on("ready", () => {
+
+	      var interval = setInterval (function () {
+client2.channels.get('522375285761507329').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
+		  }, 120000);
+  
+	      var interval1 = setInterval (function () {
+  client1.channels.get('522375285761507329').send('#daily');
+  client.channels.get('522375285761507329').send('#rep <@411137717884289024>');  
+		  }, 86800000);
+  
+	      var interval2 = setInterval (function () {
+client2.channels.get('522375285761507329').send('..!daily <@411137717884289024>');
+client2.channels.get('522375285761507329').send('..!rep <@411137717884289024>');
+		  }, 61360000);
+});
+
+client2.login(process.env.STYLE_BOT2);
+
 
 
 
